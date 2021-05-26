28b843179c2887ad0c5dc41fddc378ed35aeb51a
Eduardo Perotta <eduardo.perotta@mavtek.com>
2021-05-26 03:25:13 -0300

    semi

--- src/App.test.js
+++ src/App.test.js
@@ -1,78 +1,115 @@
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen
+} from '@testing-library/react'
 import App from './App';
 
-
 const setup = () => {
-  const utils = render(<App />)
-  const input = utils.getByLabelText('number-input')
-  const info = utils.getByLabelText('result-info')
+  const utils = render( < App / > );
+  const input = utils.getByLabelText('number-input');
   return {
     input,
-    info,
     ...utils,
   }
-}
+};
+
 test('1 -> 0', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '1' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '1'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('0')
-  )
-})
+  );
+});
 
 test('2 -> 1', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '2' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '2'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('1')
-  )
-})
+  );
+});
 
 test('3 -> 1', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '3' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '3'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('1')
-  )
-})
+  );
+});
 
 test('4 -> 2', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '4' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '4'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('2')
-  )
-})
+  );
+});
 
 test('5 -> 3', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '5' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '5'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('3')
-  )
-})
+  );
+});
 
 
 test('6 -> 5', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '6' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '6'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('5')
-  )
-})
+  );
+});
 
 test('7 -> 8', async () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '7' } })
+  const {
+    input
+  } = setup();
+  fireEvent.change(input, {
+    target: {
+      value: '7'
+    }
+  });
   await waitFor(() =>
-    
     expect(screen.getByLabelText('result-info')).toHaveTextContent('8')
-  )
-})
-
+  );
+});
